b089dcc19a9c8abee2efa9151dae613c84d94da5
Eferurhobo Temple voke <starukido@gmail.com>
2022-08-30 22:18:06 +0100

    api

--- firstapi.js
+++ firstapi.js
@@ -1,14 +1,50 @@
-fetch('https://reqres.in/api/users',{
-    method:'POST',
-    body: JSON.stringify(
-        {
-            name: 'user 1'
-        }
-    ),
-    headers:{
-        'content-type':'application/json'
-    }
 
+
+
+
+document.querySelector('.entertainment').addEventListener("click",(e)=>{
+    const category = "science"
+    const url =`https://newsapi.org/v2/top-headlines?country=ng&category=${category}&apiKey=4391f42befbb4afab31db42c05367f1d`;
+    
+    fetch(url)
+    .then(res=> res.json() )
+    .then((data)=>{
+        const {articles} = data
+        articles.forEach((article) => {
+            console.log(article)
+            let {urlToImage,title,source,description,url,publishedAt} = article
+            document.querySelector('.output').innerHTML +=`<div class="card" style="width: 18rem">
+            <img src="${urlToImage}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <h5 class="card-title text-muted">${publishedAt}</h5>
+              <p class="card-text">${description}</p>
+              <a href="${url}" class="btn btn-primary">Read more</a>
+            </div>
+          </div>`
+        });
+    })
+    
 })
-.then(res=> res.json() )
-.then(data=>console.log(data))
+
+
+
+
+document.querySelector('.technology').addEventListener("click",(e)=>{
+
+    const category = "science"
+    const url =`https://newsapi.org/v2/top-headlines?country=ng&category=${category}&apiKey=4391f42befbb4afab31db42c05367f1d`;
+    
+        fetch(url)
+        .then(res=> res.json() )
+        .then((data)=>{
+            const {articles} = data
+            articles.forEach((article) => {
+                console.log(article)
+                let {urlToImage,title,source,description,url,publishedAt} = article
+                document.querySelector('.output').innerHTML +=`<div style:"padding-top:20px;"><img style="float:left;width:150px;" src="${urlToImage}"><h1>${title}</h1>${source.name}</br>
+            ${description} <a href="${url}" target="_blank">${url}</a> </br>${publishedAt}</div>`
+            });
+        })
+})
+
